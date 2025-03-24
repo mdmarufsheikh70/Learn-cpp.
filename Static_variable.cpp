4b129c04a61c5a920bfcd965c876c046c1d6caaa
Maruf <mdmarufsheikh707@gmail.com>
2025-03-25 00:38:51 +0600

    Update Static_variable.cpp

--- Static_variable.cpp
+++ Static_variable.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 class Counter {
 public:
-    static int count; // স্ট্যাটিক ভেরিয়েবল
+    static int count; 
     Counter() {
         count++;
     }
 };
 
-int Counter::count = 0; // স্ট্যাটিক ভেরিয়েবলকে ইনিশিয়ালাইজ করা
+int Counter::count = 0; 
 
 int main() {
-    Counter c1, c2, c3,c4,c5;
+    Counter c1, c2, c3, c4, c5;
     cout << "Total objects created: " << Counter::count << endl;
     return 0;
 }
