91cee9bae53c39849c618cbcb138a99552d938f3
Maruf <mdmarufsheikh707@gmail.com>
2025-03-29 23:34:47 +0600

    Update Pointer2.cpp

--- Pointer2.cpp
+++ Pointer2.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x[20]={10,20,30,40,50};
+    int x[20] = {10,20,30,40,50};
     int *p1;
-    p1=&x[0];
-    for(int i=0;i<5;i++){
-    cout<<*p1;
+    p1 = &x[0];
+    for(int i = 0; i < 5; i++){
+    court << *p1;
     p1++;
     cout<<endl;
     }
