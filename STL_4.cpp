329d8eb5adfaf0fd23a1b48737c4e5b6eacb0e09
Maruf <mdmarufsheikh707@gmail.com>
2025-04-08 12:49:01 +0600

    Add files via upload

---
+++ STL_4.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << v[i];
+    }
+
+    vector<int> tem;
+    tem = v;
+    for(int i = 0; i < n; i++){
+        cout << tem[i];
+    }
+    return 0;
+}
