49ea560ea3e5dcb6ccf046b5369a47aeae8292db
Maruf <mdmarufsheikh707@gmail.com>
2025-04-10 20:34:56 +0600

    Add files via upload

---
+++ CSV_Write_Read.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    fstream file("example.csv", ios::in | ios::out | ios::app);
+
+    if (file.is_open()) {
+        file << "Name,Age,Country\n";
+        file << "Alice,25,USA\n";
+        file.seekg(0);
+
+        string line;
+        while (getline(file, line)) {
+            cout << line << endl;
+        }
+
+        file.close();
+    } else {
+        cout << "Cannot open file";
+    }
+
+    return 0;
+}
