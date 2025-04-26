5f26c52a42e6cb9180a8c7eb542810395f5db503
Maruf <mdmarufsheikh707@gmail.com>
2025-04-26 22:14:57 +0600

    Add files via upload

---
+++ STL_10.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+#define endl '\n';
+int main() {
+    optimize();
+   int n;
+   cin >> n;
+   cin.ignore();
+   while (n--)
+   {
+    string s;
+    getline(cin, s);
+    cout << s << endl;
+   }
+   
+    return 0;
+}
